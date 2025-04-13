7f545350c5852330314779c8edecfd45b185ea1a
Denis <denys.minich@gmail.com>
2025-04-13 14:00:03 +0000

    Added new administrators and edit permissions for them, add logout button and proper redirect to login

---
+++ web/themes/custom/budova_admin_theme/js/budova-admin-theme.js
@@ -0,0 +1,17 @@
+/**
+ * @file
+ * budova_admin_theme behaviors.
+ */
+(function (Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.budovaAdminTheme = {
+    attach (context, settings) {
+
+      console.log('It works!');
+
+    }
+  };
+
+} (Drupal));
